c5423ecd61fef847db58e4e0a53b7e8bc19aa33e
aabusha3 <aabusha3@uwo.ca>
2022-10-19 13:27:06 -0400

    Add front-end capabilities

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
+const csv = require('csv-parser');
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send('Hello World');
+// app.get('/', (req, res) =>{
+//     res.send('Hello World');
+// });
+
+app.use('/', express.static('static'));
+const genres = fs.createReadStream("./lab3-data/genres.csv");
+// app.get('/', (req, res) => {
+//     res.send('hello')
+// });
+
+app.get('/api/genres', (req, res) => {
+    genres
+    .on('error', (err) => {
+        console.log(err);
+    })
+
+    .pipe(csv())
+    .on('data', (row) => {
+        //console.log(row);
+        let str = `Genre ID: ${row["genre_id"]} Track: ${row["#tracks"]} Parent: ${row["parent"]} Title: ${row["title"]} Top Level: ${row["top_level"]}.\n`;
+        //console.log(str);
+        //res.write(JSON.stringify(row)+'\n');
+        res.write(str);
+    })
+
+    .on('end', () => {
+        res.status(200).send();
+    })
 });
 
-
-const port = process.env.PORT || 5684;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening To ${port}`))
+
+
+
+
