efda7a7d2fb181d5aad954b9d1a4179695a0df71
aabusha3 <aabusha3@uwo.ca>
2022-10-31 12:56:36 -0400

    add get detail for a given id

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const tracksFile = fs.createReadStream("./lab3-data/raw_tracks.csv");
 
 function getGenres(){}
 
+
 genresRoute.get('/', (req, res) => {
     genresFile
     .on('error', (err) => {
@@ -29,13 +30,41 @@ genresRoute.get('/', (req, res) => {
     .on('data', (row) => {
         //console.log(row);
         let str = `Genre ID: ${row["genre_id"]} Track: ${row["#tracks"]} Parent: ${row["parent"]} Title: ${row["title"]} Top Level: ${row["top_level"]}.\n`;
-        //console.log(str);
+        //console.log(row["genre_id"].toString());
         //res.write(JSON.stringify(row)+'\n');
         res.write(str);
     })
 
     .on('end', () => {
         res.status(200).send();
+    })
+});
+
+
+
+
+
+genresRoute.get('/:genre_id', (req, res) => {
+    console.log('hi1')
+    genresFile
+    .on('error', (err) => {
+        console.log(err);
+    })
+
+    .pipe(csv())
+    .on('data', (row) => {
+        console.log('hi2')
+        if(row["genre_id"] == (req.params.genre_id)){
+            console.log('found it')
+            res.write(row["genre_id"]);
+        }
+        //else {console.log(`${req.params.genre_id} not found: on ${row["genre_id"]}`)}
+        
+    })
+
+    .on('end', () => {
+        console.log('hi9')
+        return res.end();
     })
 });
 
