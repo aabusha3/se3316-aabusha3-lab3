7144cc430d6d69e15b451a6f4d6535a571e092f2
aabusha3 <aabusha3@uwo.ca>
2022-11-01 13:29:33 -0400

    fix dupelication of items in step 1

--- static/call.js
+++ static/call.js
@@ -4,8 +4,8 @@ function step1(){
     fetch('/api/genres')
     .then(res => res.json()
         .then(data => {
-            console.log(data);
             const ul = document.getElementById('step1Result');
+            while(ul.firstChild) ul.removeChild(ul.firstChild);
             data.forEach(d => {
                 const li = document.createElement('li');
                 li.appendChild(document.createTextNode(`Name: ${d.title} ID: ${d.genre_id} Parent: ${d.parent}`))
