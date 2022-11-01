b892c47e45de86145238ec3e1cd61a6a4bfee61b
aabusha3 <aabusha3@uwo.ca>
2022-11-01 16:22:30 -0400

    add full CRUD methods for artist csv file

--- app.js
+++ app.js
@@ -34,8 +34,9 @@ genresRoute.route('/')
     })
     .post((req, res) => {
         const newID = req.body;
-        newID.genre_id = genresArr.length;
-        if(!(newID['#tracks'] && newID.parent && newID.title && newID.top_level)) res.status(404).send('please make sure all parts of the genre are present in your request');
+        newID.genre_id = genresArr.length+1;
+        if(!(newID['#tracks'] && newID.parent && newID.title && newID.top_level)) 
+            res.status(404).send('please make sure all parts of the genre are present in your request');
         else {
             genresArr[parseInt(newID.genre_id)] = newID;
             res.send(newID);
@@ -44,21 +45,21 @@ genresRoute.route('/')
 
 genresRoute.route('/:genre_id')
     .get((req, res) => {
-        const id = genresArr.find(g => parseInt(g['genre_id']) === parseInt(req.params.genre_id));
+        const id = genresArr.find(g => parseInt(g.genre_id) === parseInt(req.params.genre_id));
         if(id) res.send(id);
         else res.status(404).send(`Genre ID ${req.params.genre_id} was not found`);
     })
     .put((req, res) => {
         const newID = req.body;
         newID.genre_id = parseInt(req.params.genre_id);
-        const indexID = genresArr.findIndex(g => parseInt(g['genre_id']) === parseInt(newID.genre_id));
+        const indexID = genresArr.findIndex(g => parseInt(g.genre_id) === parseInt(newID.genre_id));
         if(indexID < 0) genresArr.push(newID);
         else genresArr[indexID] = newID;
         res.send(newID);
     })
     .post((req, res) => {
         const newID = req.body;
-        const indexID = genresArr.findIndex(g => parseInt(g['genre_id']) === parseInt(req.params.genre_id));
+        const indexID = genresArr.findIndex(g => parseInt(g.genre_id) === parseInt(req.params.genre_id));
         if(indexID < 0) res.status(404).send(`Genre ID ${req.params.genre_id} was not found`);
         else {
             if(newID['#tracks']) genresArr[indexID]['#tracks'] = parseInt(genresArr[indexID]['#tracks']) + parseInt(newID['#tracks']);
@@ -69,11 +70,66 @@ genresRoute.route('/:genre_id')
         }
     })
     .delete((req, res) => {
-        const indexID = genresArr.findIndex(g => parseInt(g['genre_id']) === parseInt(req.params.genre_id));
-        if(indexID < 0) res.status(404).send(`${req.params.genre_id} does not exist`);
+        const indexID = genresArr.findIndex(g => parseInt(g.genre_id) === parseInt(req.params.genre_id));
+        if(indexID < 0) res.status(404).send(`Genre ID ${req.params.genre_id} does not exist`);
         else {
-            res.send(`Removed ${req.params.genre_id}`);
+            res.send(`Removed Genre ID ${req.params.genre_id}`);
             genresArr.splice(indexID, 1);
+        }
+    });
+
+
+artistsRoute.route('/')
+    .get((req, res) => {
+    res.send(artistsArr);
+    })
+    .post((req, res) => {
+        const newID = req.body;
+        newID.artist_id = artistsArr.length+1;
+        if(!(newID.artist_name && newID.artist_handle && newID.tags && newID.artist_url 
+            && newID.artist_favorites && newID.artist_comments  && newID.artist_date_created)) 
+            res.status(404).send('please make sure all parts of the artist are present in your request');
+        else {
+            artistsArr[parseInt(newID.artist_id)] = newID;
+            res.send(newID);
+        }
+    });
+
+artistsRoute.route('/:artist_id')
+    .get((req, res) => {
+        const id = artistsArr.find(r => parseInt(r.artist_id) === parseInt(req.params.artist_id));
+        if(id) res.send(id);
+        else res.status(404).send(`Artist ID ${req.params.artist_id} was not found`);
+    })
+    .put((req, res) => {
+        const newID = req.body;
+        newID.artist_id = parseInt(req.params.artist_id);
+        const indexID = artistsArr.findIndex(r => parseInt(r.artist_id) === parseInt(newID.artist_id));
+        if(indexID < 0) artistsArr.push(newID);
+        else artistsArr[indexID] = newID;
+        res.send(newID);
+    })
+    .post((req, res) => {
+        const newID = req.body;
+        const indexID = artistsArr.findIndex(r => parseInt(r.artist_id) === parseInt(req.params.artist_id));
+        if(indexID < 0) res.status(404).send(`Artist ID ${req.params.artist_id} was not found`);
+        else {
+            if(newID.artist_name) artistsArr[indexID].artist_name += (' ' + newID.artist_name);
+            if(newID.artist_handle) artistsArr[indexID].artist_handle += (' ' + newID.artist_handle);
+            if(newID.tags) artistsArr[indexID].tags += (' ' + newID.tags);
+            if(newID.artist_url) artistsArr[indexID].artist_url += (' ' + newID.artist_url);
+            if(newID.artist_favorites) artistsArr[indexID].artist_favorites = parseInt(artistsArr[indexID].artist_favorites) + parseInt(newID.artist_favorites);
+            if(newID.artist_comments) artistsArr[indexID].artist_comments = parseInt(artistsArr[indexID].artist_comments) + parseInt(newID.artist_comments);
+            if(newID.artist_date_created) artistsArr[indexID].artist_date_created += (' ' + newID.artist_date_created);
+            res.send(newID);
+        }
+    })
+    .delete((req, res) => {
+        const indexID = artistsArr.findIndex(r => parseInt(r.artist_id) === parseInt(req.params.artist_id));
+        if(indexID < 0) res.status(404).send(`Artist ID ${req.params.artist_id} does not exist`);
+        else {
+            res.send(`Removed Artist ID ${req.params.artist_id}`);
+            artistsArr.splice(indexID, 1);
         }
     });
 
