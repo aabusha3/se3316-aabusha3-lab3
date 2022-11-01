ee8cfda104783ea8194e98250ec4b2370504bb55
aabusha3 <aabusha3@uwo.ca>
2022-11-01 00:32:14 -0400

    convert all genre CRUD methods to be used w/ arrays instead of readstream

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const artistsRoute = express.Router();
 const albumsRoute = express.Router();
 const tracksRoute = express.Router();
 
-const genresFile = fs.createReadStream("./lab3-data/genres.csv");
+const genresArr = new Array();
+fs.createReadStream("./lab3-data/genres.csv").pipe(csv()).on('data', (data) => genresArr.push(data));
 const artistsFile = fs.createReadStream("./lab3-data/raw_artists.csv");
 const albumsFile = fs.createReadStream("./lab3-data/raw_albums.csv");
 const tracksFile = fs.createReadStream("./lab3-data/raw_tracks.csv");
@@ -22,65 +23,22 @@ genresRoute.use(express.json());
 
 
 genresRoute.get('/', (req, res) => {
-    genresFile
-    .on('error', (err) => {
-        console.log(err);
-    })
-
-    .pipe(csv())
-    .on('data', (row) => {
-        //console.log(row);
-        let str = `Genre ID: ${row["genre_id"]} Track: ${row["#tracks"]} Parent: ${row["parent"]} Title: ${row["title"]} Top Level: ${row["top_level"]}.\n`;
-        //console.log(row["genre_id"].toString());
-        //res.write(JSON.stringify(row)+'\n');
-        res.send('<p>'+str+'</p>');
-    })
-
-    .on('end', () => {
-        res.status(200).send();
-    })
+   res.send(genresArr);
 });
 
+
 genresRoute.get('/s1', (req, res) => {
-    genresFile
-    .on('error', (err) => {
-        console.log(err);
-    })
-
-    .pipe(csv())
-    .on('data', (row) => {
-        let str = `Genre Names: ${row["title"]}   Genre ID: ${row["genre_id"]}   Parent ID: ${row["parent"]}.\n`;
-        res.send(JSON.stringify(str));
-    })
-
-    .on('end', () => {
-        res.status(200).send();
-    })
+    const s1_genresArr = new Array(); 
+    for(g of genresArr)
+        s1_genresArr.push({Genre_Names:g['title'], Genre_ID:g['genre_id'], Parent_ID:g['parent']});
+    res.send(s1_genresArr);
 });
 
 
 genresRoute.get('/:genre_id', (req, res) => {
-    console.log('hi1')
-    genresFile
-    .on('error', (err) => {
-        console.log(err);
-    })
-
-    .pipe(csv())
-    .on('data', (row) => {
-        console.log('hi2')
-        if(parseInt(row["genre_id"]) === parseInt(req.params.genre_id)){
-            console.log('found it')
-            res.send(JSON.stringify(row["genre_id"]));
-        }
-        //else {console.log(`${req.params.genre_id} not found: on ${row["genre_id"]}`)}
-        
-    })
-
-    .on('end', () => {
-        console.log('hi9')
-        return res.end();
-    })
+    const id = genresArr.find(i => parseInt(i['genre_id']) === parseInt(req.params.genre_id));
+    if(id) res.send(id);
+    else res.status(404).send(`Genre ID ${req.params.genre_id} was not found`);
 });
 
 // genresRoute.put('/:genre_id', (req, res) => {
@@ -88,28 +46,28 @@ genresRoute.get('/:genre_id', (req, res) => {
 // });
 
 
-artistsRoute.get('/s2/:artist_id', (req, res) => {
-    console.log('hi1')
-    artistsFile
-    .on('error', (err) => {
-        console.log(err);
-    })
+// artistsRoute.get('/s2/:artist_id', (req, res) => {
+//     console.log('hi1')
+//     artistsFile
+//     .on('error', (err) => {
+//         console.log(err);
+//     })
 
-    .pipe(csv())
-    .on('data', (row) => {
-        console.log('hi2')
-        if(parseInt(row["artist_id"]) === parseInt(req.params.artist_id)){
-            console.log('found it')
-            let str = `Artist ID: ${row["artist_id"]} Date Created: ${row["artist_date_created"]} Handle: ${row["artist_handle"]} Name: ${row["artist_name"]} Tags: ${row["tags"]} Favorites: ${row["artist_favorites"]}.\n`;
-            res.send(JSON.stringify(str));
-        }        
-    })
+//     .pipe(csv())
+//     .on('data', (row) => {
+//         console.log('hi2')
+//         if(parseInt(row["artist_id"]) === parseInt(req.params.artist_id)){
+//             console.log('found it')
+//             let str = `Artist ID: ${row["artist_id"]} Date Created: ${row["artist_date_created"]} Handle: ${row["artist_handle"]} Name: ${row["artist_name"]} Tags: ${row["tags"]} Favorites: ${row["artist_favorites"]}.\n`;
+//             res.send(JSON.stringify(str));
+//         }        
+//     })
 
-    .on('end', () => {
-        console.log('hi9')
-        return res.end();
-    })
-});
+//     .on('end', () => {
+//         console.log('hi9')
+//         return res.end();
+//     })
+// });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening To ${port}`))
