44441cb7ea16257266c9fda431e2e78c96ff1064
aabusha3 <aabusha3@uwo.ca>
2022-11-01 21:44:45 -0400

    implement frontend for step 2

--- static/call.js
+++ static/call.js
@@ -1,5 +1,4 @@
 document.getElementById('step1').addEventListener('click', step1);
-
 function step1(){
     fetch('/api/genres')
     .then(res => res.json()
@@ -13,3 +12,22 @@ function step1(){
             });
     }));
 }
+
+document.getElementById('step2').addEventListener('click', step2);
+function step2(){
+    fetch(`/api/artists/${document.getElementById('id_step2').value}`)
+    .then(res => res.json()
+        .then(d => {
+            const li = document.getElementById('step2Result');
+            li.innerHTML = '';
+            li.appendChild(document.createTextNode(`ID: ${d.artist_id} Name: ${d.artist_name} Handle: ${d.artist_handle} Tags: ${d.tags} URL: ${d.artist_url} Favorites: ${d.artist_favorites} Comments: ${d.artist_comments} Date Created: ${d.artist_date_created}`))
+    }));
+}
+
+// artistsRoute.get('/s2/:artist_id', (req, res) => {
+//     const id = artistsArr.find(r => parseInt(r['artist_id']) === parseInt(req.params.artist_id));
+//     if(id) res.send(JSON.stringify({artist_id:id.artist_id, artist_name:id.artist_name, 
+//          artist_handle:id.artist_handle, tags:id.tags, artist_url:id.artist_url, 
+//          artist_favorites:id.artist_favorites}));
+//     else res.status(404).send(`Artist ID ${req.params.artist_id} was not found`);
+// });
