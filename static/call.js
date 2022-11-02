5b000af588e397e403d36afe2919052acda86cfc
aabusha3 <aabusha3@uwo.ca>
2022-11-02 15:15:51 -0400

    implement frontend for step 4

--- static/call.js
+++ static/call.js
@@ -1,5 +1,12 @@
 document.getElementById('step1').addEventListener('click', step1);
+document.getElementById('step1_alt').addEventListener('click', step1Clear);
+function step1Clear(){
+    const ul = document.getElementById('step1Result');
+    while(ul.firstChild) ul.removeChild(ul.firstChild);
+    document.getElementById('step1Status').innerText = 'Genres Cleared Succesufully';
+}
 function step1(){
+    document.getElementById('step1Status').innerText = '';
     fetch('/api/genres')
     .then(res => res.json()
         .then(data => {
@@ -11,7 +18,9 @@ function step1(){
                 ul.appendChild(li);
             });
             document.getElementById('step1Status').innerText = 'Genres Displayed Succesufully';
-    }))
+        })
+        .catch(err => document.getElementById('step1Status').innerText = `Genres List Data Not Found\t${err}`)
+    )
     .catch(err => document.getElementById('step1Status').innerText = `Genres List Could Not Be Found\t${err}`);
 }
 
@@ -20,11 +29,14 @@ document.getElementById('step2').addEventListener('click', step2);
 function step2(){
     const id = parseInt(document.getElementById('id_step2').value);
     const li = document.getElementById('step2Result');
+    document.getElementById('step3Status').innerText = '';
     li.innerHTML = '';
     fetch(`/api/artists/${id}`)
     .then(res => res.json()
         .then(d => {
-            li.appendChild(document.createTextNode(`ID: ${d.artist_id} Name: ${d.artist_name} Handle: ${d.artist_handle} Tags: ${d.tags} URL: ${d.artist_url} Favorites: ${d.artist_favorites} Comments: ${d.artist_comments} Date Created: ${d.artist_date_created}`))
+            li.appendChild(document.createTextNode(`ID: ${d.artist_id} Name: ${d.artist_name} 
+            Handle: ${d.artist_handle} Tags: ${d.tags} URL: ${d.artist_url} Favorites: ${d.artist_favorites} 
+            Comments: ${d.artist_comments} Date Created: ${d.artist_date_created}`))
             document.getElementById('step2Status').innerText = `Artist ${id} Found`;
         })
         .catch(err => document.getElementById('step2Status').innerText = `Artist ${id} Not Found\t${err}`)
@@ -37,6 +49,7 @@ document.getElementById('step3').addEventListener('click', step3);
 function step3(){
     const id = parseInt(document.getElementById('id_step3').value);
     const li = document.getElementById('step3Result');
+    document.getElementById('step3Status').innerText = '';
     li.innerHTML = '';
     fetch(`/api/tracks/${id}`)
     .then(res => res.json()
@@ -51,4 +64,33 @@ function step3(){
         .catch(err => document.getElementById('step3Status').innerText = `Track ${id} Not Found\t${err}`)
     )
     .catch(err => document.getElementById('step3Status').innerText = `Tracks List Could Not Be Found\t${err}`);
+}
+
+
+document.getElementById('step4').addEventListener('click', step4);
+function step4(){
+    let max = 12;
+    const tt = document.getElementById('tt_step4').value.toLowerCase();
+    const at = document.getElementById('at_step4').value.toLowerCase();
+    const ul = document.getElementById('step4Result');
+    while(ul.firstChild) ul.removeChild(ul.firstChild);
+    document.getElementById('step4Status').innerText = 'Searching The Archives, Please Be Patient';
+
+    fetch(`/api/tracks`)
+    .then(res => res.json()
+        .then(data => {
+            for(d of data){
+                if((tt.length>0 && d.track_title.toLowerCase().includes(tt)) || (at.length>0 && d.album_title.toLowerCase().includes(at))){
+                    const li = document.createElement('li');
+                    li.appendChild(document.createTextNode(`Track ID: ${d.track_id}`))
+                    ul.appendChild(li);
+                    max--;
+                }
+                if(max === 0) break;
+            }
+            document.getElementById('step4Status').innerText = 'Track IDs Displayed Succesufully';
+        })
+        .catch(err => document.getElementById('step4Status').innerText = `Tracks List Data Not Found\t${err}`)
+    )
+    .catch(err => document.getElementById('step4Status').innerText = `Tracks List Could Not Be Found\t${err}`);
 }
