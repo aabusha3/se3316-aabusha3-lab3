8626ef4c0c0f54c809090123ac37eac94fcf4dcb
aabusha3 <aabusha3@uwo.ca>
2022-11-01 23:07:02 -0400

    fix dupe issue

--- static/call.js
+++ static/call.js
@@ -12,23 +12,23 @@ function step1(){
             });
             document.getElementById('step1Status').innerText = 'Genres Displayed Succesufully';
     }))
-    .catch(err => document.getElementById('step1Status').innerText = `Genres List Could Not Be Found\n${err}`);
+    .catch(err => document.getElementById('step1Status').innerText = `Genres List Could Not Be Found\t${err}`);
 }
 
 document.getElementById('step2').addEventListener('click', step2);
 function step2(){
     const id = parseInt(document.getElementById('id_step2').value);
+    const li = document.getElementById('step2Result');
+    li.innerHTML = '';
     fetch(`/api/artists/${id}`)
     .then(res => res.json()
         .then(d => {
-            const li = document.getElementById('step2Result');
-            li.innerHTML = '';
             li.appendChild(document.createTextNode(`ID: ${d.artist_id} Name: ${d.artist_name} Handle: ${d.artist_handle} Tags: ${d.tags} URL: ${d.artist_url} Favorites: ${d.artist_favorites} Comments: ${d.artist_comments} Date Created: ${d.artist_date_created}`))
             document.getElementById('step2Status').innerText = `Artist ${id} Found`;
         })
         .catch(err => document.getElementById('step2Status').innerText = `Artist ${id} Not Found\n${err}`)
     )
-    .catch(err => document.getElementById('step2Status').innerText = `Artists List Could Not Be Found\n${err}`)
+    .catch(err => document.getElementById('step2Status').innerText = `Artists List Could Not Be Found\t${err}`)
 }
 
 // artistsRoute.get('/s2/:artist_id', (req, res) => {
