286a0fa82c5659d42055458f9f8a4ea080b87f57
aabusha3 <aabusha3@uwo.ca>
2022-11-04 02:42:14 -0400

    add 'createTextNode()' to further sanatize user input

--- static/call.js
+++ static/call.js
@@ -7,14 +7,14 @@ document.getElementById('step1_alt').addEventListener('click', step1Clear);
 function step1Clear(){
     const ul = document.getElementById('step1Result');
     while(ul.firstChild) ul.removeChild(ul.firstChild);
-    document.getElementById('step1Status').innerText = 'Genres Cleared Successfully';
+    document.getElementById('step1Status').innerText = document.createTextNode('Genres Cleared Successfully').textContent;
 }
 document.getElementById('step1').addEventListener('click', step1);
 function step1(){
     const status = document.getElementById('step1Status');
     const ul = document.getElementById('step1Result');
     while(ul.firstChild) ul.removeChild(ul.firstChild);
-    status.innerText = '';
+    status.innerText = document.createTextNode('').textContent;
 
     fetch('/api/genres')
     .then(res => res.json()
@@ -24,11 +24,11 @@ function step1(){
                 li.appendChild(document.createTextNode(`Name: ${d.title} ID: ${d.genre_id} Parent: ${d.parent}`))
                 ul.appendChild(li);
             });
-            status.innerText = 'Genres Displayed Successfully';
+            status.innerText = document.createTextNode('Genres Displayed Successfully').textContent;
         })
-        .catch(err => status.innerText = `Genres List Data Not Found\t${err}`)
+        .catch(err => status.innerText = document.createTextNode(`Genres List Data Not Found`).textContent)
     )
-    .catch(err => status.innerText = `Genres List Could Not Be Found\t${err}`);
+    .catch(err => status.innerText = document.createTextNode(`Genres List Could Not Be Found`).textContent);
 }
 
 
@@ -36,14 +36,14 @@ document.getElementById('step2_alt').addEventListener('click', step2Clear);
 function step2Clear(){
     const li = document.getElementById('step2Result');
     li.innerHTML = '';
-    document.getElementById('step2Status').innerText = "Artist's Info Cleared Successfully";
+    document.getElementById('step2Status').innerText = document.createTextNode("Artist's Info Cleared Successfully").textContent;
 }
 document.getElementById('step2').addEventListener('click', step2);
 function step2(){
     const status = document.getElementById('step2Status');
     const id = parseInt(document.getElementById('id_step2').value);
     const li = document.getElementById('step2Result');
-    status.innerText = '';
+    status.innerText = document.createTextNode('').textContent;
     li.innerHTML = '';
 
     fetch(`/api/artists/${id}`)
@@ -52,11 +52,11 @@ function step2(){
             li.appendChild(document.createTextNode(`ID: ${d.artist_id} Name: ${d.artist_name} 
             Handle: ${d.artist_handle} Tags: ${d.tags} URL: ${d.artist_url} Favorites: ${d.artist_favorites} 
             Comments: ${d.artist_comments} Date Created: ${d.artist_date_created}`))
-            status.innerText = `Artist ${id} Found`;
+            status.innerText = document.createTextNode(`Artist ${id} Found`).textContent;
         })
-        .catch(err => status.innerText = `Artist ${id} Not Found\t${err}`)
+        .catch(err => status.innerText = document.createTextNode(`Artist ${id} Not Found`).textContent)
     )
-    .catch(err => status.innerText = `Artists List Could Not Be Found\t${err}`);
+    .catch(err => status.innerText = document.createTextNode(`Artists List Could Not Be Found`).textContent);
 }
 
 
@@ -64,14 +64,14 @@ document.getElementById('step3_alt').addEventListener('click', step3Clear);
 function step3Clear(){
     const li = document.getElementById('step3Result');
     li.innerHTML = '';
-    document.getElementById('step3Status').innerText = "Track's Info Cleared Successfully";
+    document.getElementById('step3Status').innerText = document.createTextNode("Track's Info Cleared Successfully").textContent;
 }
 document.getElementById('step3').addEventListener('click', step3);
 function step3(){
     const status = document.getElementById('step3Status');
     const id = parseInt(document.getElementById('id_step3').value);
     const li = document.getElementById('step3Result');
-    status.innerText = '';
+    status.innerText = document.createTextNode('').textContent;
     li.innerHTML = '';
 
     fetch(`/api/tracks/${id}`)
@@ -82,11 +82,11 @@ function step3(){
             Date Created: ${d.track_date_created} Date Recorded: ${d.track_date_recorded} 
             Duration: ${d.track_duration} Genres: ${d.track_genres} Track#: ${d.track_number} 
             Track Title: ${d.track_title}`))
-            status.innerText = `Track ${id} Found`;
+            status.innerText = document.createTextNode(`Track ${id} Found`).textContent;
         })
-        .catch(err => status.innerText = `Track ${id} Not Found\t${err}`)
+        .catch(err => status.innerText = document.createTextNode(`Track ${id} Not Found`).textContent)
     )
-    .catch(err => status.innerText = `Tracks List Could Not Be Found\t${err}`);
+    .catch(err => status.innerText = document.createTextNode(`Tracks List Could Not Be Found`).textContent);
 }
 
 
@@ -94,7 +94,7 @@ document.getElementById('step4_alt').addEventListener('click', step4Clear);
 function step4Clear(){
     const ul = document.getElementById('step4Result');
     while(ul.firstChild) ul.removeChild(ul.firstChild);
-    document.getElementById('step4Status').innerText = 'Filetered Track Ids Cleared Successfully';
+    document.getElementById('step4Status').innerText = document.createTextNode('Filetered Track Ids Cleared Successfully').textContent;
 }
 document.getElementById('step4').addEventListener('click', step4);
 function step4(){
@@ -104,7 +104,7 @@ function step4(){
     const at = strip(document.getElementById('at_step4').value).toLowerCase();
     const ul = document.getElementById('step4Result');
     while(ul.firstChild) ul.removeChild(ul.firstChild);
-    status.innerText = 'Searching The Archives, Please Be Patient';
+    status.innerText = document.createTextNode('Searching The Archives, Please Be Patient').textContent;
 
     fetch('/api/tracks')
     .then(res => res.json()
@@ -118,11 +118,11 @@ function step4(){
                 }
                 if(max === 0) break;
             }
-            status.innerText = 'Track IDs Displayed Successfully';
+            status.innerText = document.createTextNode('Track IDs Displayed Successfully').textContent;
         })
-        .catch(err => status.innerText = `Tracks List Data Not Found\t${err}`)
+        .catch(err => status.innerText = document.createTextNode(`Tracks List Data Not Found`).textContent)
     )
-    .catch(err => status.innerText = `Tracks List Could Not Be Found\t${err}`);
+    .catch(err => status.innerText = document.createTextNode(`Tracks List Could Not Be Found`).textContent);
 }
 
 
@@ -130,7 +130,7 @@ document.getElementById('step5_alt').addEventListener('click', step5Clear);
 function step5Clear(){
     const ul = document.getElementById('step5Result');
     while(ul.firstChild) ul.removeChild(ul.firstChild);
-    document.getElementById('step5Status').innerText = 'Filetered Artist Ids Cleared Successfully';
+    document.getElementById('step5Status').innerText = document.createTextNode('Filetered Artist Ids Cleared Successfully').textContent;
 }
 document.getElementById('step5').addEventListener('click', step5);
 function step5(){
@@ -138,7 +138,7 @@ function step5(){
     const name = strip(document.getElementById('name_step5').value).toLowerCase();
     const ul = document.getElementById('step5Result');
     while(ul.firstChild) ul.removeChild(ul.firstChild);
-    status.innerText = 'Searching The Archives, Please Be Patient';
+    status.innerText = document.createTextNode('Searching The Archives, Please Be Patient').textContent;
 
     fetch('/api/artists')
     .then(res => res.json()
@@ -150,11 +150,11 @@ function step5(){
                     ul.appendChild(li);
                 }
             });
-            status.innerText = 'Artist IDs Displayed Successfully';
+            status.innerText = document.createTextNode('Artist IDs Displayed Successfully').textContent;
         })
-        .catch(err => status.innerText = `Artists List Data Not Found\t${err}`)
+        .catch(err => status.innerText = document.createTextNode(`Artists List Data Not Found`).textContent)
     )
-    .catch(err => status.innerText = `Artists List Could Not Be Found\t${err}`);
+    .catch(err => status.innerText = document.createTextNode(`Artists List Could Not Be Found`).textContent);
 }
 
 
@@ -162,12 +162,12 @@ document.getElementById('step6').addEventListener('click', step6);
 function step6(){
     const status = document.getElementById('step69Status');
     const name = strip(document.getElementById('name_step6').value);
-    status.innerText = '';
+    status.innerText = document.createTextNode('').textContent;
 
     fetch(`/api/lists/create/${name}`)
     .then(res => res.json()
         .then(data => {
-            status.innerText = data;
+            status.innerText = document.createTextNode(data).textContent;
     }));
 }
 
@@ -175,12 +175,12 @@ document.getElementById('step9').addEventListener('click', step9);
 function step9(){
     const status = document.getElementById('step69Status');
     const name = strip(document.getElementById('name_step6').value);
-    status.innerText = '';
+    status.innerText = document.createTextNode('').textContent;
 
     fetch(`/api/lists/delete/${name}`)
     .then(res => res.json()
         .then(data => {
-            status.innerText = data;
+            status.innerText = document.createTextNode(data).textContent;
     }));
 }
 
@@ -190,12 +190,12 @@ function step7(){
     const status = document.getElementById('step7Status');
     const name = strip(document.getElementById('name_step7').value);
     const id = parseInt(document.getElementById('id_step7').value);
-    status.innerText = '';
+    status.innerText = document.createTextNode('').textContent;
 
     fetch(`/api/lists/write/${name}/${id}`)
     .then(res => res.json()
         .then(data => {
-            status.innerText = data;
+            status.innerText = document.createTextNode(data).textContent;
     }));
 }
 
@@ -204,7 +204,7 @@ document.getElementById('step8_alt').addEventListener('click', step8Clear);
 function step8Clear(){
     const ul = document.getElementById('step8Result');
     while(ul.firstChild) ul.removeChild(ul.firstChild);
-    document.getElementById('step8Status').innerText = 'List Cleared Successfully';
+    document.getElementById('step8Status').innerText = document.createTextNode('List Cleared Successfully').textContent;
 }
 document.getElementById('step8').addEventListener('click', step8);
 function step8(){
@@ -224,15 +224,15 @@ function step8(){
                     Album ID: ${d.album_id} Album Name: ${d.album_title}`));
                     ul.appendChild(li);
                 });
-                status.innerText = 'Ids In List Displayed Successfully';
+                status.innerText = document.createTextNode('Ids In List Displayed Successfully').textContent;
             }
             else if(res.status === 404){
-                status.innerText = data;
+                status.innerText = document.createTextNode(data).textContent;
             }
         })
-        .catch(err => status.innerText = `List Data Not Found\t${err}`)
+        .catch(err => status.innerText = document.createTextNode(`List Data Not Found`).textContent)
     )
-    .catch(err => status.innerText = `List Could Not Be Found\t${err}`);
+    .catch(err => status.innerText = document.createTextNode(`List Could Not Be Found`).textContent);
 }
 
 
@@ -241,14 +241,14 @@ document.getElementById('step10_alt').addEventListener('click', step10Clear);
 function step10Clear(){
     const ul = document.getElementById('step10Result');
     while(ul.firstChild) ul.removeChild(ul.firstChild);
-    document.getElementById('step10Status').innerText = 'Genres Cleared Successfully';
+    document.getElementById('step10Status').innerText = document.createTextNode('Genres Cleared Successfully').textContent;
 }
 document.getElementById('step10').addEventListener('click', step10);
 function step10(){
     const status = document.getElementById('step10Status');
     const ul = document.getElementById('step10Result');
     while(ul.firstChild) ul.removeChild(ul.firstChild);
-    status.innerText = '';
+    status.innerText = document.createTextNode('').textContent;
 
     fetch('/api/lists/list')
     .then(res => res.json()
@@ -258,9 +258,9 @@ function step10(){
                 li.appendChild(document.createTextNode(`Name: ${d.name} ID Count: ${d.length} Duration: ${d.duration}`))
                 ul.appendChild(li);
             });
-            status.innerText = 'Lists Displayed Successfully';
+            status.innerText = document.createTextNode('Lists Displayed Successfully').textContent;
         })
-        .catch(err => status.innerText = `Lists Data Not Found\t${err}`)
+        .catch(err => status.innerText = document.createTextNode(`Lists Data Not Found`))
     )
-    .catch(err => status.innerText = `Lists Could Not Be Found\t${err}`);
+    .catch(err => status.innerText = document.createTextNode(`Lists Could Not Be Found`).textContent);
 }
